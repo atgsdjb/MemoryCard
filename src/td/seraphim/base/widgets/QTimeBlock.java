f28f6a356ee5f5c0627811f9ac9e1cc2835d348d
dongjinbin <td.seraphim@gmail.com>
2013-03-06 22:05:48 +0800

    Change-Id: I62ddca3a6a4e011ec1a9d4c85c9886be3243cf9f Signed-off-by: dongjinbin <td.seraphim@gmail.com>

---
+++ src/td/seraphim/base/widgets/QTimeBlock.java
@@ -0,0 +1,20 @@
+package td.seraphim.base.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class QTimeBlock extends View {
+
+	public QTimeBlock(Context context){
+		super(context);
+	}
+	public QTimeBlock(Context context,AttributeSet attrs){
+		super(context,attrs);
+	}
+	public QTimeBlock(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
